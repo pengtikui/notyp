6bd031f4a6aa014bc2c145e512c79a8ed6170945
pengtikui <949828390@qq.com>
2021-07-10 22:56:08 +0800

    docs: update example

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,6 +1,16 @@
 import 'reflect-metadata';
-import { Application, Controller, Get, Injectable, Query } from '@notyp/core';
+import {
+  Application,
+  Body,
+  Controller,
+  Injectable,
+  Post,
+  Query,
+  ValidationException,
+} from '@notyp/core';
 import config from '@notyp/config';
+import { Transform } from 'class-transformer';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 @Injectable()
 class UserService {
@@ -9,16 +19,49 @@ class UserService {
   }
 }
 
+class UserQuery {
+  @IsNotEmpty()
+  @Transform(({ value }) => Number(value) || 0)
+  id: number;
+}
+
+class UserBody {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+}
+
 @Controller('user')
 class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Get('detail')
-  getUserDetail(@Query() query: any) {
-    return { ...this.userService.getUserDetail(), query, dbConfig: config.get('DB') };
+  @Post('detail')
+  getUserDetail(@Query() query: UserQuery, @Body() body: UserBody) {
+    return {
+      ...this.userService.getUserDetail(),
+      query,
+      body,
+      dbConfig: config.get('DB'),
+    };
   }
 }
 
-const app = new Application({ controllers: [UserController] });
+const errorHandler = async (ctx: any, next: any) => {
+  try {
+    await next();
+  } catch (error) {
+    if (error instanceof ValidationException) {
+      ctx.status = 400;
+      ctx.body = '参数校验失败';
+    } else {
+      ctx.status = 500;
+      ctx.body = 'Internal Server Error';
+    }
+  }
+};
 
-app.bootstrap(8000);
+const app = new Application({ controllers: [UserController], middlewares: [errorHandler] });
+
+app.bootstrap(8000, () => {
+  console.log('server is runing at http://localhost:8000');
+});
