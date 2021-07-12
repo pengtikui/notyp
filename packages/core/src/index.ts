4de8483d53c030e5417b87897d6aa40a48802b96
pengtikui <949828390@qq.com>
2021-07-12 22:21:23 +0800

    feat(core): add types export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,9 @@
 import 'reflect-metadata';
 
+export { Context, Next, Middleware } from 'koa';
+export { Options as BodyParserOptions } from 'koa-bodyparser';
+export { opts as SessionOptions } from 'koa-session';
+
 export * from './application';
 export * from './decorator/injectable';
 export * from './decorator/controller';
